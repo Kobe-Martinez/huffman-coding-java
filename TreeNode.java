4d1c5299e04a52ae87d23af8e2c4494e48d8b8a2
Kobe Martinez <kobe.martinez@myyahoo.com>
2025-01-28 02:47:58 -0500

    Update TreeNode.java

--- TreeNode.java
+++ TreeNode.java
@@ -5,7 +5,7 @@ package huffman;
  * This class represents a node of a huffman coding tree,
  * and contains a CharFreq object as its data
  * 
- * @author Ishaan Ivaturi
+ * @author Kobe Martinez
  */
 public class TreeNode {
     private CharFreq data;
